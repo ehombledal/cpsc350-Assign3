753e988106ee7ee3952ec15c8b22a680a2f2227b
Erik Hombledal <hombledal@chapman.edu>
2020-04-03 23:21:31 +0000

    Initial Commit

---
+++ Checker_main.cpp
@@ -0,0 +1,83 @@
+#include "genStack.h"
+#include <fstream>
+
+int stackSize = 100;
+
+int main(int argc, char **argv)
+{
+  bool cont = true;
+  bool failedTest = false;
+
+  string fileName;
+  string line;
+  char toCheck;
+
+  while (cont)
+  {
+    genStack<char> *stack = new genStack<char>(stackSize);
+    int lineNum = 0;
+
+    cout << "what is the name or path of the file you want analyzed?" << endl;
+    cin >> fileName;
+
+    inFS.open(fileName);
+    if (!inFS.is_open())
+    {
+      cout << "could not open file." << endl;
+      cout << "please enter a correct file name and try again." << endl;
+      break;
+    }
+
+    while (!inFS.eof() && !failedTest)
+    {
+      inFS >> line;
+      line++;
+      if(!inFS.fail() && !failedTest)
+      {
+        for (int i = 0; i < line.length(); ++i)
+        {
+          if (line[i] == '(' || line[i] == '[' || line[i] =='{')
+          {
+            stack -> push(line[i]);
+          }
+
+          if (line[i] == ')')
+          {
+            char toCheck = stack -> pop();
+            if (toCheck != '(')
+            {
+              cout << "Error at line " << lineNum << " Expected (" << endl;
+              failedTest = true;
+              break;
+            }
+          }
+
+          if (line[i] == '[')
+          {
+            char toCheck = stack -> pop();
+            if (toCheck != '[')
+            {
+              cout << "Error at line " << lineNum << " Expected [" << endl;
+              failedTest = true;
+              break;
+            }
+          }
+
+          if (line[i] == '{')
+          {
+            char toCheck = stack -> pop();
+            if (toCheck != '{')
+            {
+              cout << "Error at line " << lineNum << " Expected {" << endl;
+              failedTest = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+    cout << "file done! Would you like to test another file? (true/false)" << endl;
+    cin << cont;
+  }
+  cout << "thank you for using the Syntax Checker! Exiting..." << endl; 
+}
